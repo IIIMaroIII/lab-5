40763a5b20a569f88adce07e0daaf9e3d8489d48
rod <matveev.r.v@icloud.com>
2025-12-03 01:49:45 +0200

    quick_commit

---
+++ src/main/java/lt/esdc/stringOps/StringOps.java
@@ -0,0 +1,19 @@
+package lt.esdc.stringOps;
+
+import com.github.javafaker.Faker;
+
+public class StringOps {
+    public static void main(String[] args) {
+init();
+    }
+
+    static void init() {
+
+    }
+
+    public String randomSentences(int quantity, char delimiter) {
+        Faker faker = new Faker();
+
+        
+    }
+}
